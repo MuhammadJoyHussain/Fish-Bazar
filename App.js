fa76695c2572ebb7b080ff010280326b978074d9
unknown <muhammadjoyhussain@gmail.comdtrujhtyjnjjtdjhnghj>
2021-06-04 00:11:58 +0600

    updated

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Starting React Native Team Project......!</Text>
+      <Text>Cloning app</Text>
       <StatusBar style="auto" />
     </View>
   );
