fdf05810f243cc0cced6e8e0c980a186f4102391
Hasan Siam <hasansiam4545@gmail.com>
2021-06-04 00:06:52 +0600

    changed

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Starting React Native Team Project!</Text>
+      <Text>Starting React Native Team Project......!</Text>
       <StatusBar style="auto" />
     </View>
   );
